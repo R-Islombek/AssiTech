952a5e64a0538a1308b24e064e052295e55abd8a
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-11-03 23:12:01 +0500

    countup qoshish

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useTranslation } from "react-i18next";
 import Header from './components/Header/Header';
 import Main from './components/Main/main';
+import Services from './components/Services/Services';
 import "./App.css"
 import Hero from './components/Hero/Hero';
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
          <Header/>
          <Hero/>
          <Main/>
+         <Services/>
     </div>
   )
 }
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { useTranslation } from "react-i18next";
 import "./Services.css"
 const Services = () => {
+      const { t } = useTranslation();
   return (
       <div className='services'>
             <div className='container'>
                  <div className='services__container'>
-                       
+                       <div className='services__left'>
+                            <h3 className='services__title'>{t("services.title")}</h3>
+                            <p className='services__text'>{t("services.text1")}</p>
+                       </div>  
+                       <div className='services__bottom'>
+                            <h3 className='services__title-name'>{t("services.title1")}</h3>
+                            <ul className='services__list'>
+                                <li className='services__item'>
+                                      
+                                </li>
+                            </ul>
+                       </div>
                  </div>
             </div>
       </div>
